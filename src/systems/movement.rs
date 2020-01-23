bda3c5722bbf5890decf471a6e81cacf2b59f47c
Tymoteusz Paul <puciek@gmail.com>
2020-01-23 20:08:41 +0000

    Adjusting transform by delta_frame

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,5 +1,5 @@
 extern crate amethyst;
-use amethyst::core::Transform;
+use amethyst::core::{Time, Transform};
 
 use amethyst::ecs::*;
 
@@ -10,11 +10,12 @@ pub struct OrbitalMovementSystem;
 impl<'a> System<'a> for OrbitalMovementSystem {
     type SystemData = (
         ReadStorage<'a, Velocity>,
+        Read<'a, Time>,
         WriteStorage<'a, Position>,
         WriteStorage<'a, Transform>,
     );
 
-    fn run(&mut self, (velocities, mut positions, mut transforms): Self::SystemData) {
+    fn run(&mut self, (velocities, time, mut positions, mut transforms): Self::SystemData) {
         for (velocity, position, transform) in (&velocities, &mut positions, &mut transforms).join()
         {
             position.x = 0.0 + position.angle.cos() * position.r;
@@ -22,7 +23,7 @@ impl<'a> System<'a> for OrbitalMovementSystem {
             debug!("({}, {})", position.x, position.y);
             transform.set_translation_x(position.x);
             transform.set_translation_y(position.y);
-            position.angle -= velocity.angle;
+            position.angle -= velocity.angle * time.delta_seconds();
         }
     }
 }
